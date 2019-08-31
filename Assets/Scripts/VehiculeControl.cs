fa678d218c682f8164be4e837cf9820726ac95c3
Adrien RICHARD <richardadrien0@gmail.com>
2019-08-31 23:22:42 +0200

    positions

---
+++ Assets/Scripts/VehiculeControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculeControl : MonoBehaviour
+{
+    public GameObject MotoPrefab;
+
+    public directionEnum direction;
+
+    public enum directionEnum
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(direction);
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            direction = directionEnum.Right;
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            direction = directionEnum.Left;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            direction = directionEnum.Up;
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            direction = directionEnum.Down;
+        }
+    }
+}
